705949a9258f88df6b6087491d5b343fbf59f172
Canoziia <canoziia@projectk.org>
2026-01-03 02:50:47 +0800

    add cache

--- flake.nix
+++ flake.nix
@@ -1,6 +1,11 @@
 {
   description = "Canoziia's Custom Linux Kernel";
 
+  nixConfig = {
+    extra-substituters = [ "https://nix-cache.projectk.org" ];
+    extra-trusted-public-keys = [ "canoziia:qOX9w17KWcw6tKNpC6AF/dJDhybUcVYboKCpumctbVo=" ];
+  };
+
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
@@ -12,35 +17,39 @@
         "x86_64-linux"
         "aarch64-linux"
       ];
-      forAllSystems = nixpkgs.lib.genAttrs systems;
+
+      ath12kConfig = {
+        name = "ath12k-enable-6ghz";
+        patch = null;
+        extraConfig = ''
+          CFG80211_CERTIFICATION_ONUS y
+          ATH_REG_DYNAMIC_USER_REG_HINTS y
+          ATH_REG_DYNAMIC_USER_CERT_TESTING y
+        '';
+      };
     in
     {
-      packages = forAllSystems (
+      # 1. 导出 Overlay：这是给 NixOS 用户使用的最佳方式
+      overlays.default = final: prev: {
+        canoziiaKernel = prev.linuxPackages_latest.kernel.override {
+          kernelPatches = prev.linuxPackages_latest.kernel.kernelPatches ++ [ ath12kConfig ];
+        };
+        linuxPackages_canoziia = final.linuxPackagesFor final.canoziiaKernel;
+      };
+
+      # 2. 导出 Packages：用于 CI 构建和测试
+      packages = nixpkgs.lib.genAttrs systems (
         system:
         let
-          pkgs = nixpkgs.legacyPackages.${system};
-
-          ath12kConfig = {
-            name = "ath12k-enable-6ghz";
-            patch = null;
-            extraConfig = ''
-              CFG80211_CERTIFICATION_ONUS y
-              ATH_REG_DYNAMIC_USER_REG_HINTS y
-              ATH_REG_DYNAMIC_USER_CERT_TESTING y
-            '';
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ self.overlays.default ];
+            config.allowUnfree = true;
           };
-
-          originalKernel = pkgs.linuxPackages_latest.kernel;
-
-          customKernel = originalKernel.override {
-            kernelPatches = originalKernel.kernelPatches ++ [ ath12kConfig ];
-          };
-
-          customKernelPackages = pkgs.linuxPackagesFor customKernel;
         in
         {
-          default = customKernelPackages;
-          kernel = customKernel;
+          default = pkgs.linuxPackages_canoziia;
+          kernel = pkgs.canoziiaKernel;
         }
       );
     };
